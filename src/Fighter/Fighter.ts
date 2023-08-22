43be1c607244a194b5ea6f4c10e7a7917bfd3cf1
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-08-22 20:24:18 -0300

    Feat: Implementa a interface SimpleFighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,13 +1,12 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
-  lifePoints: number;
-  strength: number;
+export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void;
   special?(enemy: Fighter): void;
   levelUp():void;
-  receiveDamage(attackPoints: number): number;
 }
+
+export { SimpleFighter };
